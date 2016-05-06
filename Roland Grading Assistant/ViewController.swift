e8609fce0b952476412575545ea0195605e7bcf5
jimmy <jimmylroland@gmail.com>
2016-05-05 20:47:07 -0500

    Initial Commit

---
+++ Roland Grading Assistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roland Grading Assistant
+//
+//  Created by Jimmy Roland on 5/5/16.
+//  Copyright © 2016 Jimmy Roland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
